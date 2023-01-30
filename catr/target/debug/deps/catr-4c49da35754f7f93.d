47319156dd292f77156587fc6885bbefe547a220
Luxum-Dev <78885336+Luxum-Dev@users.noreply.github.com>
2023-01-30 23:31:03 +0100

    Delete catr directory

--- catr/target/debug/deps/catr-4c49da35754f7f93.d
+++
@@ -1,5 +0,0 @@
-/home/luxum/Documents/rust_cli/catr/target/debug/deps/catr-4c49da35754f7f93.rmeta: src/main.rs
-
-/home/luxum/Documents/rust_cli/catr/target/debug/deps/catr-4c49da35754f7f93.d: src/main.rs
-
-src/main.rs:
